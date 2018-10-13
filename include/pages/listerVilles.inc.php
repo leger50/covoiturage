6c0e05768bab0438756058fb8110489dde6db7a7
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-10-13 19:22:45 +0200

    add css listerVille

--- include/pages/listerVilles.inc.php
+++ include/pages/listerVilles.inc.php
@@ -4,21 +4,24 @@ $villeManager = new VilleManager($pdo);
 $villes = $villeManager->getAllVilles();
 ?>
 
-<h1>Lister des villes</h1>
-<p>Actuellement <?php echo count($villes)?> villes sont enregistrées</p>
+<div id="listerVille">
+  <h1>Lister des villes</h1>
 
-<table>
+  <p>Actuellement <?php echo count($villes)?> villes sont enregistrées</p>
 
-  <tr>
-    <th>Numéro</th>
-    <th>Nom</th>
-  <tr>
+  <table>
 
-  <?php foreach($villes as $ville) { ?>
     <tr>
-      <td><?php echo $ville->getNumVille()?></td>
-      <td><?php echo $ville->getNomVille()?></td>
-    </tr>
-  <?php } ?>
+      <th>Numéro</th>
+      <th>Nom</th>
+    <tr>
 
-</table>
+    <?php foreach($villes as $ville) { ?>
+      <tr>
+        <td><?php echo $ville->getNumVille()?></td>
+        <td><?php echo $ville->getNomVille()?></td>
+      </tr>
+    <?php } ?>
+
+  </table>
+</div>
