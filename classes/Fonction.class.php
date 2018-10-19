fa8268c24209cd4cbeb280f91549d8284288b8ff
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-10-19 11:22:13 +0200

    add fonction

--- classes/Fonction.class.php
+++ classes/Fonction.class.php
@@ -1,4 +1,45 @@
 <?php
 class Fonction{
-	
+	private $num;
+	private $libelle;
+
+	public function __construct($valeurs){
+		if(!empty($valeurs)){
+			$this->affecte($valeurs);
+		}
+	}
+
+	public function affecte($valeurs){
+		foreach($valeurs as $attribut => $valeur){
+
+			switch($attribut){
+				case 'fon_num' :
+					$this->setNumFonction($valeur);
+					break;
+
+				case 'fon_libelle' :
+					$this->setLibelleFonction($valeur);
+					break;
+			}
+		}
+	}
+
+	public function getNumFonction(){
+		return $this->num;
+	}
+
+	public function setNumFonction($num){
+		if(is_numeric($num)){
+			$this->num = $num;
+		}
+	}
+
+	public function getLibelleFonction(){
+		return $this->libelle;
+	}
+
+	public function setLibelleFonction($libelle){
+		$this->libelle = $libelle;
+	}
+
 }
--- classes/FonctionManager.class.php
+++ classes/FonctionManager.class.php
@@ -1,4 +1,23 @@
 <?php
 class FonctionManager{
-	
+	private $db;
+
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	public function getAllFonctions(){
+
+		$sql = 'SELECT fon_num, fon_libelle FROM fonction';
+
+		$requete = $this->db->prepare($sql);
+		$requete->execute();
+
+		while($fonction = $requete->fetch(PDO::FETCH_OBJ)){
+			$listeFonctions[] = new Fonction($fonction);
+		}
+
+		$requete->closeCursor();
+		return $listeFonctions;
+	}
 }
