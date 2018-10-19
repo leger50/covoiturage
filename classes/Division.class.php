75aa4c964f5febe10247bb181564a3144af33ec9
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-10-19 10:58:34 +0200

    add classe division

--- classes/Division.class.php
+++ classes/Division.class.php
@@ -1,4 +1,45 @@
 <?php
 class Division{
-	
+	private $num;
+	private $nom;
+
+	public function __construct($valeurs){
+		if(!empty($valeurs)){
+			$this->affecte($valeurs);
+		}
+	}
+
+	public function affecte($valeurs){
+		foreach($valeurs as $attribut => $valeur){
+
+			switch($attribut){
+				case 'div_num' :
+					$this->setNumDivision($valeur);
+					break;
+
+				case 'div_nom' :
+					$this->setNomVille($valeur);
+					break;
+			}
+		}
+	}
+
+	public function getNumDivision(){
+		return $this->num;
+	}
+
+	public function setNumDivision($num){
+		if(is_numeric($num)){
+			$this->num = $num;
+		}
+	}
+
+	public function getNomVille(){
+		return $this->nom;
+	}
+
+	public function setNomVille($nom){
+		$this->nom = $nom;
+	}
+
 }
--- classes/DivisionManager.class.php
+++ classes/DivisionManager.class.php
@@ -1,4 +1,23 @@
 <?php
 class DivisionManager{
-	
+	private $db;
+
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	public function getAllDivisions(){
+
+		$sql = 'SELECT div_num, div_nom FROM division';
+
+		$requete = $this->db->prepare($sql);
+		$requete->execute();
+
+		while($division = $requete->fetch(PDO::FETCH_OBJ)){
+			$listeDivisions[] = new Division($division);
+		}
+
+		$requete->closeCursor();
+		return $listeDivisions;
+	}
 }
