ab51185bb26144a98742298853626fd1f86b7aa4
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-10-19 10:58:56 +0200

    Add function encrypted

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -4,11 +4,17 @@
 		$date = $membres[2].'-'.$membres[1].'-'.$membres[0];
 		return $date;
 	}
-	
+
 	function addJours($date, $nbJours){
 		$membres = explode('/', $date);
 		$date = $membres[2].'-'.$membres[1].'-'.(intval($membres[0])+$nbJours);
 		return $date;
 	}
-	
+
+	function createProtectedPassword($pwd){
+    $protectedPassword = sha1(sha1($pwd).SALT);
+
+		return $protectedPassword;
+	}
+
 ?>
