81c6fd06f6b95cd2c184315f3a8d157f7798d25a
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-10-19 11:02:03 +0200

    add departement

--- classes/Departement.class.php
+++ classes/Departement.class.php
@@ -1,4 +1,60 @@
 <?php
 class Departement{
-	
+	private $num;
+	private $nom;
+	private $villeNum
+
+	public function __construct($valeurs){
+		if(!empty($valeurs)){
+			$this->affecte($valeurs);
+		}
+	}
+
+	public function affecte($valeurs){
+		foreach($valeurs as $attribut => $valeur){
+
+			switch($attribut){
+				case 'dep_num' :
+					$this->setNumDepartement($valeur);
+					break;
+
+				case 'dep_nom' :
+					$this->setNomDepartement($valeur);
+					break;
+
+				case 'vil_num' :
+					$this->setNumVilleDivision($valeur);
+					break;
+			}
+		}
+	}
+
+	public function getNumDepartement(){
+		return $this->num;
+	}
+
+	public function setNumDepartement($num){
+		if(is_numeric($num)){
+			$this->num = $num;
+		}
+	}
+
+	public function getNomDepartement(){
+		return $this->nom;
+	}
+
+	public function setNomDepartement($nom){
+		$this->nom = $nom;
+	}
+
+	public function getNumVilleDivision(){
+		return $this->villeNum;
+	}
+
+	public function setNumVilleDivision($villeNum){
+		if(is_numeric($villeNum)){
+			$this->villeNum = $villeNum;
+		}
+	}
+
 }
--- classes/DepartementManager.class.php
+++ classes/DepartementManager.class.php
@@ -1,4 +1,23 @@
 <?php
 class DepartementManager{
-	
+	private $db;
+
+	public function __construct($db){
+		$this->db = $db;
+	}
+
+	public function getAllDepartements(){
+
+		$sql = 'SELECT dep_num, dep_nom, vil_num FROM departement';
+
+		$requete = $this->db->prepare($sql);
+		$requete->execute();
+
+		while($departement = $requete->fetch(PDO::FETCH_OBJ)){
+			$listeDepartements[] = new Departement($departement);
+		}
+
+		$requete->closeCursor();
+		return $listeDepartements;
+	}
 }
