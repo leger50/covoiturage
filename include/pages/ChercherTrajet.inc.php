a7b0ca820f7d4262e6eac2b996c131454f4d9b80
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-10-29 16:14:28 +0100

    add droits pages 'trajet'

--- include/pages/ChercherTrajet.inc.php
+++ include/pages/ChercherTrajet.inc.php
@@ -1,1 +1,8 @@
-<h1>Rechercher un trajet</h1>
+<?php if(isset($_SESSION['login'])){?>
+  <h1>Rechercher un trajet</h1>
+
+<?php }else{ ?>
+  <p>Vous devez être connecté pour accéder à cette page !</p>
+  <p><img class = 'icone' src='image/erreur.png' alt='Erreur accès'> Redirection automatique dans 3 secondes</p>
+  <?php header("Refresh: 3;URL=index.php");
+}?>
