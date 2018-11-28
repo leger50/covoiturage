691e13b833fcae52cf772988d48b49c8366d6077
Leger Charlie <charlie.leger@etu.unilim.fr>
2018-11-28 11:07:45 +0100

    passage en mode prod

--- include/config.inc.php
+++ include/config.inc.php
@@ -6,7 +6,7 @@ define('DBHOST', "localhost");
 define('DBNAME', "covoiturage");
 define('DBUSER', "bd");
 define('DBPASSWD', "bede");
-define('ENV','dev');
+define('ENV','prod');
 define('DBPORT',3306);
 define('SALT','48@!alsd');
 define('PRECISION', 3);
